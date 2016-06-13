d70d1f6e7954dba77976ced300f4a2bf0e232002
littlehawk93 <jch101@latech.edu>
2016-06-13 09:20:48 -0500

    adding more library JS functions. building more structure

--- js/main.js
+++ js/main.js
@@ -1,24 +1,27 @@
 
-var deck = [];
+var currentGame = {
+    deck: [],
+    players: []
+};
 
 function shuffleDeck(numDecks = 1) {
     
     var suites = ["spades", "clubs", "diamonds", "hearts"];
     var values = ["ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king"];
     
-    deck = [];
+    currentGame.deck = [];
     
     for(n=0; n<numDecks; n++) {
         $.each(suites, function(i, suite) {
             $.each(values, function(i, value){
 
                 if(value == "ace") {
-                    deck.push({
+                    currentGame.deck.push({
                         class: "card " + suite + " " + value,
                         value: [ 1, 10 ]
                     });
                 } else {
-                    deck.push({
+                    currentGame.deck.push({
                         class: "card " + suite + " " + value,
                         value: [ ((i + 1 > 10) ? 10 : i + 1) ]
                     });
@@ -29,22 +32,41 @@ function shuffleDeck(numDecks = 1) {
     
     var i, j, obj;
     
-    for(i = deck.length; i; i -= 1) {
+    for(i = currentGame.deck.length; i; i -= 1) {
         j = Math.floor(Math.random() * i);
-        obj = deck[i - 1];
-        deck[i - 1] = deck[j];
-        deck[j] = obj;
+        obj = currentGame.deck[i - 1];
+        currentGame.deck[i - 1] = currentGame.deck[j];
+        currentGame.deck[j] = obj;
     }
+}
+
+function addCardToStack(cardStackZone) {
+    
+}
+
+function addPlayer(player) {
+    
+    currentGame.players.push(player);
+    renderPlayers();
+}
+
+function renderPlayers() {
+    
+    $("#player-zones").empty();
+    
+    var numPlayers = currentGame.players.length;
+    
+    $.each(currentGame.players, function(i, player) {
+        
+        if(i == 0 && numPlayers * 3 < )
+    });
+}
+
+function newGame() {
+    
 }
 
 $(document).ready(function() {
     
-    shuffleDeck(5);
-    
-    $.each(deck, function(i, card) {
-        
-        $("<div>", {
-            class: card.class
-        }).appendTo("#main-content #house-zone");
-    });
+    shuffleDeck();
 });
