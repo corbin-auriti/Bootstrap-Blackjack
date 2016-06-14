629e13bd1dab25ea2863eb3c61a77b06c3f3ace9
littlehawk93 <littlehawk93@gmail.com>
2016-06-13 22:21:20 -0500

    added more library code and basic structure for web page player card layouts

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 
-var currentGame = {
-    deck: [],
-    players: []
-};
+var currentGame = {};
 
+// Shuffles the deck, like casinos, deck can be shuffled with multiple decks to make counting cards more difficult
 function shuffleDeck(numDecks = 1) {
     
+    // The names of each suit and card value. Used for generating the appropriate CSS classes to render card faces
     var suites = ["spades", "clubs", "diamonds", "hearts"];
     var values = ["ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king"];
     
@@ -18,12 +17,14 @@ function shuffleDeck(numDecks = 1) {
                 if(value == "ace") {
                     currentGame.deck.push({
                         class: "card " + suite + " " + value,
-                        value: [ 1, 10 ]
+                        values: [ 1, 10 ],
+                        value: 1
                     });
                 } else {
                     currentGame.deck.push({
                         class: "card " + suite + " " + value,
-                        value: [ ((i + 1 > 10) ? 10 : i + 1) ]
+                        values: [ ((i + 1 > 10) ? 10 : i + 1) ],
+                        value: ((i + 1 > 10) ? 10 : i + 1)
                     });
                 }
             });
@@ -40,13 +41,22 @@ function shuffleDeck(numDecks = 1) {
     }
 }
 
-function addCardToStack(cardStackZone) {
-    
+function dealCardToPlayer(card, player) {
+
+    player.hand.push(card);
 }
 
+// Render a new card to a card stack zone
+function addCardToStack(card, cardStackZone) {
+    
+
+}
+
+// Add a new player to the game
 function addPlayer(player) {
     
     currentGame.players.push(player);
+    
     renderPlayers();
 }
 
@@ -57,15 +67,65 @@ function renderPlayers() {
     var numPlayers = currentGame.players.length;
     
     $.each(currentGame.players, function(i, player) {
-        
-        if(i == 0 && numPlayers * 3 < )
+        if(currentGame.env.firstRender == 1) {
+            if(numPlayers == 1) {
+                addPlayerZone(player, "col-xs-4 col-xs-offset-4");
+            } else if (numPlayers == 2) {
+                addPlayerZone(player, "col-xs-3 col-xs-offset-2");
+            } else if (numPlayers == 3) {
+                addPlayerZone(player, "col-xs-3 col-xs-offset-1");
+            } else if (numPlayers == 4) {
+                addPlayerZone(player, "col-xs-3");
+            }
+        } else {
+            renderPlayer(player);
+        }
     });
+
+    if(currentGame.env.firstRender == 1) {
+        currentGame.env.firstRender == 0;
+    }
 }
 
+function addPlayerZone(player, className) {
+
+    $("<div id='" + player.id + "' class='" + className + " player-zone'><div class='container-fluid'><div class='row'><div class='col-xs-12 card-stack-zone stack-up'></div></div><div class='row'><div class='col-xs-12'><h2 class='text-center player-name'></h2><h2 class='text-center'><span class='current-points'>0</span> <small>pts</small></h2><h4> class='text-center value'></h4></div></div></div></div>").appendTo("#player-zones");
+}
+
+function renderPlayer(player) {
+
+    $(player.id + " .player-name").html(player.name);
+
+    var currentPts = 0;
+
+    $.each(player.hand, function(i, card) {
+        
+        currentPts += card.value;
+    });
+
+    $(player.id + " .current-points").html(currentPts);
+}
+
+// Reset current game and create a new game with the newly loaded players and game settings
 function newGame() {
     
+    currentGame = {
+        deck: [],
+        players: [],
+        deckIndex: 0,
+        settings: {
+            background: "/img/background.jpg",
+            numberOfDecks: 1
+        },
+        env: {
+            firstRender: 1
+        }
+    };
+
+    shuffleDeck(currentGame.settings.numberOfDecks);
 }
 
+// Initialize the page
 $(document).ready(function() {
     
     shuffleDeck();
